000c0c54b21c2a341da4e9c6c4b9b24b6ca837f7
tracer-ics2018 <tracer@njuics.org>
2019-07-12 04:14:45 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u3 (2019-06-16) x86_64 GNU/Linux  04:14:45 up  3:17,  1 user,  load average: 0.12, 0.11, 0.09 79e4ea91b667aecb27456dcc150ec784617d8b3c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -204,13 +204,13 @@ uint32_t eval(int p, int q) {
 		/* The expression is surrounded by a matched pair of parentheses.
 		 * If that is the case, just throw away the parentheses.
 		 */
-		return eval(p + 1, q - 1);
+		return eval(p + 1, q - 1);printf("==: %d, %d",p + 1, q-1);
 	}
 	else {
 		/* We should do more things here. */
 		int op = dominant_operator(p , q);
-		uint32_t val1 = eval(p, op - 1);
-	    uint32_t val2 = eval(op + 1, q);
+		uint32_t val1 = eval(p, op - 1);printf("val1: %d, %d",p, op-1);
+	    uint32_t val2 = eval(op + 1, q);printf("val2: %d, %d",op + 1, q);
 		switch (tokens[op].type) {
 		    case '+': printf("'+', %d\n", val1 + val2); return val1 + val2;
 			case '-': printf("'-', %d\n", val1 - val2); return val1 - val2;
