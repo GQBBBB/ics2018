d56d22be36417823af985345c6d005dc9c2be1dd
tracer-ics2018 <tracer@njuics.org>
2019-07-12 05:36:23 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u3 (2019-06-16) x86_64 GNU/Linux  05:36:23 up  4:39,  1 user,  load average: 0.10, 0.14, 0.09 7a1b31c173278976c059671e64a176bfecc7a50e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -209,13 +209,13 @@ uint32_t eval(int p, int q) {
 	}
 	else {
 		/* We should do more things here. */
-		int op = dominant_operator(p , q);
+		int op = dominant_operator(p, q);
     	uint32_t val1 = eval(p, op - 1);
 		uint32_t val2 = eval(op + 1, q);
 		switch (tokens[op].type) {
-		    case '+': printf("'+', %d\n", val1 + val2); return val1 + val2;
-			case '-': printf("'-', %d\n", val1 - val2); return val1 - val2;
-		    case '*': printf("'*', %d\n", val1 * val2); return val1 * val2;
+		    case '+': return val1 + val2;
+			case '-': return val1 - val2;
+		    case '*': return val1 * val2;
 		    case '/': if(val2 == 0)
 						  panic("表达式（%d, %d）结果为零！", op + 2, q + 1);
 					  else
