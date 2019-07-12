b76c86e950bc987744377591045caffb1dd24310
tracer-ics2018 <tracer@njuics.org>
2019-07-12 04:32:05 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u3 (2019-06-16) x86_64 GNU/Linux  04:32:05 up  3:34,  1 user,  load average: 0.07, 0.10, 0.09 7391bac9cd1dc74139a4c8565fdc15f98d3d748

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -204,7 +204,7 @@ uint32_t eval(int p, int q) {
 		/* The expression is surrounded by a matched pair of parentheses.
 		 * If that is the case, just throw away the parentheses.
 		 */
-	    printf("==: %d, %d",p + 1, q-1);
+	    printf("==: %d, %d\n",p + 1, q-1);
 	    return eval(p + 1, q - 1);
 	}
 	else {
