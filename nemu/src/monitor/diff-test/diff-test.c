1ad197e62557748ea693838ee96ee75c6c4bcbc8
tracer-ics2018 <tracer@njuics.org>
2019-10-05 08:28:18 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u3 (2019-06-16) x86_64 GNU/Linux  08:28:18 up 18:25,  3 users,  load average: 0.02, 0.07, 0.02 5d26c7cb13f3332084316a3beb36ba44e0dfb41e

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -71,5 +71,17 @@ void difftest_step(uint32_t eip) {
 
   // TODO: Check the registers state with the reference design.
   // Set `nemu_state` to `NEMU_ABORT` if they are not the same.
-  TODO();
+  if (ref_r.eax != cpu.eax || ref_r.ecx != cpu.ecx || ref_r.edx != cpu.edx || ref_r.ebx != cpu.ebx || ref_r.esp != cpu.esp || ref_r.ebp != cpu.ebp || ref_r.esi != cpu.esi || ref_r.edi != cpu.edi || ref_r.eip != cpu.eip) {
+    nemu_state = NEMU_ABORT;
+
+  printf("qemu eax:0x%08x, nemu eax:0x%08x, eip:0x%08x\n", ref_r.eax, cpu.eax, cpu.eip);
+  printf("qemu ecx:0x%08x, nemu ecx:0x%08x, eip:0x%08x\n", ref_r.ecx, cpu.ecx, cpu.eip);
+  printf("qemu edx:0x%08x, nemu edx:0x%08x, eip:0x%08x\n", ref_r.edx, cpu.edx, cpu.eip);
+  printf("qemu ebx:0x%08x, nemu ebx:0x%08x, eip:0x%08x\n", ref_r.ebx, cpu.ebx, cpu.eip);
+  printf("qemu esp:0x%08x, nemu esp:0x%08x, eip:0x%08x\n", ref_r.esp, cpu.esp, cpu.eip);
+  printf("qemu ebp:0x%08x, nemu ebp:0x%08x, eip:0x%08x\n", ref_r.ebp, cpu.ebp, cpu.eip);
+  printf("qemu esi:0x%08x, nemu esi:0x%08x, eip:0x%08x\n", ref_r.esi, cpu.esi, cpu.eip);
+  printf("qemu edi:0x%08x, nemu edi:0x%08x, eip:0x%08x\n", ref_r.edi, cpu.edi, cpu.eip);
+  printf("qemu eip:0x%08x, nemu eip:0x%08x, eip:0x%08x\n", ref_r.eip, cpu.eip, cpu.eip);
+  }  
 }
