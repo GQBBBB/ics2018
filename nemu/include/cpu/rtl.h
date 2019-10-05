961c961cc3da5f6e53b89466bf884a46b0b59fe8
tracer-ics2018 <tracer@njuics.org>
2019-10-05 11:54:53 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u3 (2019-06-16) x86_64 GNU/Linux  11:54:53 up 21:52,  3 users,  load average: 0.06, 0.08, 0.02 240ec54d831e922d6c0d6c59294331db3285bf7

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -170,16 +170,16 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
-  rtl_subi(&cpu.esp,&cpu.esp,4);
   // M[esp] <- src1
-  rtl_sm(&cpu.esp,src1,4);
+  cpu.esp -= 4;
+  vaddr_write((cpu.esp), (*src1), 4);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
-  rtl_lm(dest,&cpu.esp,4);
   // esp <- esp + 4
-  rtl_addi(&cpu.esp,&cpu.esp,4);
+  *dest = vaddr_read(cpu.esp, 4);
+  cpu.esp += 4;
 }
 
 static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
