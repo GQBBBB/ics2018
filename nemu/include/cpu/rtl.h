5b91f73ee0e960db6478981c5b53cce777e73d26
tracer-ics2018 <tracer@njuics.org>
2019-10-06 07:36:25 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-9-amd64 #1 SMP Debian 4.9.168-1+deb9u3 (2019-06-16) x86_64 GNU/Linux  07:36:25 up 1 day, 17:33,  3 users,  load average: 0.14, 0.16, 0.09 6e2b83c7583ac0518c9dd8b39869432aee2743f4

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -194,7 +194,7 @@ static inline void rtl_setrelopi(uint32_t relop, rtlreg_t *dest,
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  rtl_shri(dest, src1, width * 8 - 1);
+  *dest = (*src1 >> (width * 8 - 1)) & 0x1;
 }
 
 #define make_rtl_setget_eflags(f) \
