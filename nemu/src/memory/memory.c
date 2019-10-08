fd6e1c77e3d2fa5db895d2c5d76f787d40fde0e6
tracer-ics2018 <tracer@njuics.org>
2019-10-08 05:37:20 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  05:37:20 up  4:19,  2 users,  load average: 0.10, 0.11, 0.04 f689978a16f882c4249989e125cb55dbafea12b5

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -9,13 +9,27 @@
 
 uint8_t pmem[PMEM_SIZE];
 
+int is_mmio(paddr_t addr);
+uint32_t mmio_read(paddr_t addr, int len, int mmio_id);
+void mmio_write(paddr_t addr, int len, uint32_t data, int mmio_id);
+
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
+  // 判断该物理地址是否被映射到I/O地址空间
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1)
+	return mmio_read(addr, len, mmio_id);
+
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, uint32_t data, int len) {
+  // 判断该物理地址是否被映射到I/O地址空间
+  int mmio_id = is_mmio(addr);
+  if (mmio_id != -1)
+	mmio_write(addr, len, data, mmio_id);
+  
   memcpy(guest_to_host(addr), &data, len);
 }
 
