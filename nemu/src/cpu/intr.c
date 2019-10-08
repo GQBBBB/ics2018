d62a841c0e245cf98db5afcff4a3bf27ebc9713c
tracer-ics2018 <tracer@njuics.org>
2019-10-08 14:23:57 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  14:23:57 up 13:06,  2 users,  load average: 0.06, 0.05, 0.05 879a58e7d65ab81d37c297d2c845379f2923f7ea

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -15,11 +15,11 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
 
   GateDesc gatedesc;
   vaddr_t addr = cpu.idtr.base + sizeof(GateDesc) * NO;
-  gatedesc.offset_15_0 = vaddr_read(addr, 4);
-  gatedesc.dont_care0 = vaddr_read(addr + 2, 4);
-  gatedesc.dont_care1 = vaddr_read(addr + 4, 4) & 0x7ffff;
-  gatedesc.present = vaddr_read(addr + 4, 4) & 0x8000;
-  gatedesc.offset_31_16 = vaddr_read(addr + 6, 4);
+  gatedesc.offset_15_0 = vaddr_read(addr, 2);
+  gatedesc.dont_care0 = vaddr_read(addr + 2, 2);
+  gatedesc.dont_care1 = vaddr_read(addr + 4, 2) & 0x7fff;
+  gatedesc.present = vaddr_read(addr + 4, 2) & 0x8000;
+  gatedesc.offset_31_16 = vaddr_read(addr + 6, 2);
   Assert(gatedesc.present, "invalid gate descriptor!");
 
   decoding.jmp_eip = (gatedesc.offset_31_16 << 16) | gatedesc.offset_15_0;
