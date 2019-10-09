3faced1331b4a29fce107bcb998e38b56dae4eff
tracer-ics2018 <tracer@njuics.org>
2019-10-09 07:37:44 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  07:37:44 up  6:03,  2 users,  load average: 0.02, 0.02, 0.00 9c953cf9edbaaee58483da156c330b97386dddda

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <x86.h>
+#include <klib.h>
 
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
@@ -9,8 +10,12 @@ void vecnull();
 _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
   if (user_handler) {
+     printf("irq=0x%x\n", tf->irq);
     _Event ev = {0};
     switch (tf->irq) {
+	  case 0x20: ev.event = _EVENT_IRQ_TIMER; break;
+      case 0x80: ev.event = _EVENT_SYSCALL; break; 
+      case 0x81: ev.event = _EVENT_YIELD; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
