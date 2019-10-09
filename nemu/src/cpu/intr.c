7c504e9e3175ffc820cf484645625f1a71436be0
tracer-ics2018 <tracer@njuics.org>
2019-10-09 02:44:30 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  02:44:30 up  1:10,  2 users,  load average: 0.07, 0.10, 0.08 24c9c8c1a65cf5b94a0424a03b2692c0b93c7094

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -20,7 +20,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   gatedesc.dont_care1 = vaddr_read(addr + 4, 2) & 0x7fff;
   gatedesc.present = 1;
   gatedesc.offset_31_16 = vaddr_read(addr + 6, 2);
-  Log("present=%d", gatedesc.present);
+  Log("=%x", vaddr_read(addr, 8));
   Assert(gatedesc.present, "invalid gate descriptor!");
 
   decoding.jmp_eip = (gatedesc.offset_31_16 << 16) | gatedesc.offset_15_0;
