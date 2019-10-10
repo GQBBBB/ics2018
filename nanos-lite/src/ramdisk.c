97830a646735f2f87a0109ac99f821683b448a16
tracer-ics2018 <tracer@njuics.org>
2019-10-10 02:16:33 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  02:16:33 up 41 min,  2 users,  load average: 0.09, 0.17, 0.14 9ccfb09861033fc5529ee0ce63032084ff392d92

--- nanos-lite/src/ramdisk.c
+++ nanos-lite/src/ramdisk.c
@@ -12,7 +12,7 @@ extern uint8_t ramdisk_end;
 /* read `len' bytes starting from `offset' of ramdisk into `buf' */
 size_t ramdisk_read(void *buf, size_t offset, size_t len) {
   assert(offset + len <= RAMDISK_SIZE);
-  Log("%x, %x", buf, ramdisk_start + offset);
+  Log("%x, %x", buf, &ramdisk_start + offset);
   memcpy(buf, &ramdisk_start + offset, len);
   return len;
 }
