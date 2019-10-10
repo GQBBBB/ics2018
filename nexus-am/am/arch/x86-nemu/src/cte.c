6729299ef14767a796949aa1383a5898f5f469fa
tracer-ics2018 <tracer@njuics.org>
2019-10-10 07:04:49 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  07:04:49 up  5:29,  2 users,  load average: 0.05, 0.10, 0.09 8f40201444d119eeaab16a69f43608781944340

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -6,6 +6,8 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 void vectrap();
 void vecnull();
+void vecsys();
+void irq0();
 
 _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
@@ -38,6 +40,9 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
   // -------------------- system call --------------------------
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
+  //
+  idt[0x80] = GATE(STS_T32A, USEL(SEG_UCODE), vecsys, DPL_USER); 
+  idt[32] = GATE(STS_TG32, KSEL(SEG_KCODE), irq0, DPL_KERN);
 
   set_idt(idt, sizeof(idt));
 
