2f82198ceb82965454662c90125e5b49c2fcff72
tracer-ics2018 <tracer@njuics.org>
2019-10-10 13:24:10 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  13:24:10 up 11:49,  2 users,  load average: 0.06, 0.04, 0.00 74c51fb51bdfd96610a61388519bdc9b1e788a88

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,12 +11,26 @@ void sys_exit(_Context *c) {
   c->GPR1 = 0;
 }
 
+void sys_write(_Context *c) {
+  int fd = c->GPR2;
+  char *buf = (char*) c->GPR3;
+  size_t len = c->GPR4; 
+  if (fd == 1 || fd == 2){
+	  for (size_t i = 0; i < len; i++){
+		  _putc(buf[i]);
+	  }
+  }
+  c->GPR1 = len;
+}
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  Log("SYSCALL_ID=0x%x", a[0]);
   switch (a[0]) {
 	case SYS_yield: sys_yield(c); break; // 1
 	case SYS_exit: sys_exit(c); break; // 0
+	case SYS_write: sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -33,7 +33,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count){
-  _exit(SYS_write);
+  _syscall_(SYS_write, fd, (intptr_t) buf, count);
   return 0;
 }
 
