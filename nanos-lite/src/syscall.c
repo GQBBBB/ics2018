c55b455dda01c68844372f99a3bfa5e37d6058e6
tracer-ics2018 <tracer@njuics.org>
2019-10-11 08:58:30 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  08:58:30 up 1 day,  7:23,  2 users,  load average: 0.17, 0.14, 0.09 93683884700d544a3d60320277cd2d70bbbbeb8e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,6 +2,11 @@
 #include "syscall.h"
 
 int mm_brk(uintptr_t new_brk);
+int fs_open(const char* pathname, int flags, int mode);
+size_t fs_write(int fd, void* buf, size_t len);
+size_t fs_read(int fd, void* buf, size_t len);
+int fs_close(int fd);
+size_t fs_lseek(int fd, size_t offset, int whence);
 
 void sys_yield(_Context *c) {
   _yield();
@@ -15,19 +20,40 @@ void sys_exit(_Context *c) {
 
 void sys_write(_Context *c) {
   int fd = c->GPR2;
-  char *buf = (char*) c->GPR3;
+  char *buf = (char *) c->GPR3;
   size_t len = c->GPR4; 
-  if (fd == 1 || fd == 2){
-	  for (size_t i = 0; i < len; i++){
-		  _putc(buf[i]);
-	  }
-  }
-  c->GPR1 = len;
+  c->GPR1 = fs_read(fd, buf, len);
+}
+
+void sys_open(_Context* c) {
+  const char* path = (const char*) c->GPR2;
+  int flags = c->GPR3;
+  int mode = c->GPR4;
+  c->GPR1 = fs_open(path, flags, mode);
 }
 
 void sys_brk(_Context *c) {
   uintptr_t addr = c->GPR2;
   c->GPR1 = mm_brk(addr);
+}
+
+void sys_read(_Context *c) {
+  int fd = c->GPR2;
+  char *buf = (char *) c->GPR3;
+  size_t len = c->GPR4;
+  c->GPR1 = fs_read(fd, buf, len); 
+}
+
+void sys_close(_Context *c) {
+  int fd = c->GPR2;
+  c->GPR1 = fs_close(fd);
+}
+
+void sys_lseek(_Context *c) {
+  int fd = c->GPR2;
+  size_t offset = c->GPR3;
+  int whence = c->GPR4;
+  c->GPR1 = fs_lseek(fd, offset, whence);
 }
 
 _Context* do_syscall(_Context *c) {
@@ -39,6 +65,10 @@ _Context* do_syscall(_Context *c) {
 	case SYS_exit: sys_exit(c); break; // 0
 	case SYS_write: sys_write(c); break; // 4
 	case SYS_brk: sys_brk(c); break; // 9
+	case SYS_open: sys_open(c); break;
+	case SYS_read: sys_read(c); break;
+	case SYS_close: sys_close(c); break;
+	case SYS_lseek: sys_lseek(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
