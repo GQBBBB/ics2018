778ff43223df1ecd777199d960ac09efe524ff30
tracer-ics2018 <tracer@njuics.org>
2019-10-11 09:00:26 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  09:00:26 up 1 day,  7:25,  2 users,  load average: 0.22, 0.15, 0.10 ac3090628c107c0785cc4da95285918ed1b3eb1a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,7 +56,7 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
-  assert(fd >2);
+  assert(fd <= 2);
 
   Finfo *file = &file_table[fd];
   
