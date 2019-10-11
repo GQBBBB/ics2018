87211b6debcc7968881beee3f5e2051c79c76089
tracer-ics2018 <tracer@njuics.org>
2019-10-11 09:23:28 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  09:23:28 up 1 day,  7:48,  2 users,  load average: 0.20, 0.27, 0.19 e0addecc4d549bdb195ca2d6d858c1b85c2e55e5

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -48,7 +48,7 @@ void *_sbrk(intptr_t increment){
 }
 
 int _read(int fd, void *buf, size_t count) {
-  return _syscall_(SYS_read, fd, (intptr_t)buf, count);;
+  return _syscall_(SYS_read, fd, (intptr_t)buf, count);
 }
 
 int _close(int fd) {
