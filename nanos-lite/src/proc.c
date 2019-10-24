411ee00fdd7ba7360b0e86fbb1d10effb03a5625
tracer-ics2018 <tracer@njuics.org>
2019-10-24 12:19:01 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  12:19:00 up 1 day, 10:37,  3 users,  load average: 0.44, 0.29, 0.16 cd3d32ac60d5bd6e553b868dd7bf1627812447a1

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,7 +29,7 @@ void init_proc() {
   //context_kload(&pcb[0], (void *)hello_fun);
   context_uload(&pcb[0], "/bin/dummy");
   //context_uload(&pcb[1], "/bin/init");
-  switch_boot_pcb();
+  switch_boot_pcb();Log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 }
 
 _Context* schedule(_Context *prev) {
