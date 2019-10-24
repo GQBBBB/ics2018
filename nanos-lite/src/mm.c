71c65ec1217c28516739040cea4c49ce0b16cc2e
tracer-ics2018 <tracer@njuics.org>
2019-10-24 06:23:54 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  06:23:54 up 1 day,  4:42,  2 users,  load average: 0.22, 0.12, 0.06 ec29f0c5c225f666106f88e6576f4913bb8fb70

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,4 +1,5 @@
 #include "memory.h"
+#include "proc.h"
 
 static void *pf = NULL;
 
@@ -19,10 +20,10 @@ int mm_brk(uintptr_t new_brk) {
   if (new_brk > current->max_brk) {
     uintptr_t vaddr_start = (current->max_brk / current->as.pgsize) * current->as.pgsize;
     
-    if (_map(&current->as, (void *)vaddr_start, NULL, MAP_TEST)) vaddr_start += current->as.pgsize;
+    if (_map(&current->as, (void *)vaddr_start, NULL, 1)) vaddr_start += current->as.pgsize;
     while (vaddr_start < new_brk) {
       void* page_base = new_page(1);
-      _map(&current->as, (void *)vaddr_start, page_base, MAP_CREATE);
+      _map(&current->as, (void *)vaddr_start, page_base, 2);
       vaddr_start += current->as.pgsize;
     }
     current->max_brk = new_brk;
