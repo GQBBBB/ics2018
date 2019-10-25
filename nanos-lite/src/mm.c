7522ad7d710a7b34afb289b6e31e43b5f58f49cf
tracer-ics2018 <tracer@njuics.org>
2019-10-25 06:31:15 +0000

    >  run 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  06:31:15 up 2 days,  4:50,  3 users,  load average: 0.82, 0.24, 0.08 b0ccc6e293f35907cdc2ccfe5c458e10d3d1b09

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -20,7 +20,9 @@ int mm_brk(uintptr_t new_brk) {
   if (new_brk > current->max_brk) {
     uintptr_t vaddr_start = (current->max_brk / current->as.pgsize) * current->as.pgsize;
     
-    if (_map(&current->as, (void *)vaddr_start, NULL, 1)) vaddr_start += current->as.pgsize;
+    if (_map(&current->as, (void *)vaddr_start, NULL, 1)) 
+	  vaddr_start += current->as.pgsize;
+
     while (vaddr_start < new_brk) {
       void* page_base = new_page(1);
       _map(&current->as, (void *)vaddr_start, page_base, 2);
