4650f4b646177814968fc102fb6b5496dafbd4cb
tracer-ics2018 <tracer@njuics.org>
2019-10-27 08:15:39 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  08:15:39 up 33 min,  2 users,  load average: 0.09, 0.60, 0.46 1e08c02f7eebd7e1973648404aef21d541299234

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,11 +35,9 @@ void init_proc() {
 _Context* schedule(_Context *prev) {
   // save the context pointer
   current->cp = prev;
-  Log("2:%x", current->cp);
   // always select pcb[0] as the new process
   current = &pcb[0];
   //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  Log("3:%x", current->cp);
   // then return the new context 
   return current->cp;
   //return prev;
