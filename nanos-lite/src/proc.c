8e88dbdd97ffaf2654036898d51f3db53a91bdaf
tracer-ics2018 <tracer@njuics.org>
2019-10-27 07:02:06 +0000

    >  compile 171220000 ics Linux cf123d4888f6 4.9.0-11-amd64 #1 SMP Debian 4.9.189-3+deb9u1 (2019-09-20) x86_64 GNU/Linux  07:02:06 up 22:19,  2 users,  load average: 0.26, 0.15, 0.11 1152df326dd355930e69bbb79b4ae10a6c4fa

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -29,18 +29,19 @@ void init_proc() {
   //context_kload(&pcb[0], (void *)hello_fun);
   context_uload(&pcb[0], "/bin/dummy");
   //context_uload(&pcb[1], "/bin/init");
+  Log("1:%x", pcb[0]->cp);
   switch_boot_pcb(); 
 }
 
 _Context* schedule(_Context *prev) {
   // save the context pointer
-  //current->cp = prev;
-  //Log("2:%x", current->cp);
+  current->cp = prev;
+  Log("2:%x", current->cp);
   // always select pcb[0] as the new process
-  //current = &pcb[0];
+  current = &pcb[0];
   //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-  //Log("3:%x", current->cp);
+  Log("3:%x", current->cp);
   // then return the new context 
-  //return current->cp;
-  return prev;
+  return current->cp;
+  //return prev;
 }
